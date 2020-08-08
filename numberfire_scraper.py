a727e8b4e0e687a27c794db4ca6034d687df641d
jmoore87jr <base_1187@yahoo.com>
2020-08-08 16:13:25 -0700

    generate lineups with pydfs and save to .csv

--- numberfire_scraper.py
+++ numberfire_scraper.py
@@ -2,15 +2,19 @@
    Draftkings export, and use pydfs-lineup-optimizer to generate lineups"""
 
 import pandas as pd
+from pydfs_lineup_optimizer import get_optimizer, Site, Sport
 
-def scrape():
-    # scrape projections from numberfire
+def get_numberfire_projections():
+    # get projections from numberfire
     list_of_dfs = pd.read_html('https://www.numberfire.com/nba/daily-fantasy/daily-basketball-projections')
     df = list_of_dfs[3]
     ##### add transformation here
     df.to_csv('projections.csv')
     print("numberfire projections saved.")
     return df 
+
+def get_sabersim_projections():
+    pass
 
 def get_dk_salaries():
     # have user input DK export URL for a slate, and download the .csv
@@ -20,6 +24,33 @@ def get_dk_salaries():
     print("Draftkings salaries saved.")
     return df
 
-scrape()
-get_dk_salaries()
+def get_lineups():
+    # already have dk_salaries.csv saved for test purposes
+    n = input("Enter the number of lineups you want: ")
+    optimizer = get_optimizer(Site.DRAFTKINGS, Sport.BASKETBALL)
+    optimizer.load_players_from_csv("dk_salaries.csv")
+    lineups = []
+    headers = ['PG', 'SG', 'SF', 'PF', 'C', 'G', 'F', 'UTIL', 'Pts', 'Salary']
+    for lineup in optimizer.optimize(n=n):
+        # create list of lineups with pts and salary tacked on
+        lineup_list = list(lineup.players)
+        lineup_list.append(lineup.fantasy_points_projection)
+        lineup_list.append(lineup.salary_costs)
+        lineups.append(lineup_list)
+    df = pd.DataFrame(lineups, columns=headers)
+    df.to_csv('lineups.csv')
+    print(f"{n} lineups have been saved.")
+    return df
+
+def main():
+    get_dk_salaries()
+    # add projections to dk_salaries sheet here
+    get_lineups()
+
+main()
+
+# get sabersim free trial and check out the format of their projections
+# do a vlookup/join type thing to add a projection column to the DK sheet
+# delete the AvgPointsPerGame column so projections replace it (keep header?)
+# feed the DK sheet with added projections into pydfs_lineup_optimizer
 
