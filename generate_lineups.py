9e257928c7ef1f4e47512b018fffd30a9b4ce729
jmoore87jr <base_1187@yahoo.com>
2020-08-10 17:54:52 -0700

    ownership projections are now working!

--- generate_lineups.py
+++ generate_lineups.py
@@ -2,10 +2,9 @@
 
 import pandas as pd
 from pydfs_lineup_optimizer import get_optimizer, Site, Sport
+from collections import defaultdict
 
-def generate_lineups():
-    site = input("Enter the site: ")
-    n = int(input("Enter the number of lineups you want: "))
+def generate_lineups(site, n):
     optimizer = get_optimizer(Site.DRAFTKINGS, Sport.BASKETBALL)
     optimizer.load_players_from_csv(f'{site}_projections.csv')
     lineups = []
@@ -16,32 +15,48 @@ def generate_lineups():
         lineup_list.append(lineup.fantasy_points_projection)
         lineup_list.append(lineup.salary_costs)
         lineups.append(lineup_list)
-    df = pd.DataFrame(lineups, columns=headers)
-    print(df['PG'].value_counts())
-    #df.to_csv(f'{site}_lineups.csv')
-    #print(f"{n} lineups have been saved to {site}_lineups.csv")
+    return pd.DataFrame(lineups, columns=headers)
 
-    # lineup outputs names with position and team
-    # do I need to separate out names? 
-    # I guess not since they'll be the same on all 3 sheets
-    # At the end we need to deal with the DK upload sheet but that's later
-    # and can maybe just be done in sheets. we'll see.
-    return df
+def calculate_exposure(): # input site names separated by comma
+    sites = input("Enter the sites to be formatted: ").replace(',', '').split()
+    n = int(input("Enter the number of lineups to generate for each site: "))
+    exposures = []
+    for site in sites:
+        df = generate_lineups(site, n)
+        positions = ['PG', 'SG', 'SF', 'PF', 'C', 'G', 'F', 'UTIL']
+        counts = df['PG'].value_counts().to_frame()
+        # join all the positions on player name so they can be summed
+        for pos in positions[1:]:
+            series = df[pos].value_counts().to_frame()
+            counts = counts.join(series, how='outer').fillna(0)
+        # create and fill the exposure column
+        site_exposure = f'{site}_exposure'
+        counts[site_exposure] = counts.sum(axis=1) / n
+        counts = counts.sort_values(by=[site_exposure], ascending=False)
+        print(f"{site} exposures: \n")
+        print(counts)
+        exposures.append(counts)
+    df = pd.concat(exposures).fillna(0)
+    d = defaultdict(list)
+    list_index = list(df.index)
+    # can't figure out how to merge the dataframes and keep ownership from each site
+    # so I have to create 2 dictionaries, one with all the values and one with the sum
+    for i,name in enumerate(list_index):
+        for site in sites:
+            col = f'{site}_exposure'
+            e = df.loc[df.index == list_index[i], col].sum()
+            d[str(name)].append(e) # pydfs objects don't behave as keys
+    d2 = defaultdict(int)
+    for k,v in d.items():
+        d2[k] = sum(v) / len(sites)
+    # convert back into DataFrame
+    results = pd.DataFrame.from_dict(d2, orient='index', 
+    columns=['projected_ownership']).sort_values(by=['projected_ownership'], ascending=False)
+    print(results)
 
-def calculate_exposure():
-    # return pd.DataFrame of players and the % of lineups they are in
-    # counter dictionary for occurences in DataFrame?
-    pass
 
-def main():
-    pass
+# upload raw .csv's to github so people can test
 
-# get sabersim projections and join them to DK sheet AvgPointsPerGame col
-  # sabersim ownership projections seem terrible?
-# feed the DK sheet with added projections into pydfs_lineup_optimizer
-# add exposure capability (guide in pydfs docs)
-"""for better ownership projections, generate 100 optimal lineups for 
-various sites and average the ownership. this will be the main function 
-of our script now."""
+calculate_exposure()
 
-generate_lineups()
+# concat all the exposure columns together, then sumif('Name' = df.index[i])
