38d7289b29443771c63c4f7d347acf5769dcaa88
jmoore87jr <base_1187@yahoo.com>
2020-09-06 08:46:19 -0700

    added lineup capability for maximizing ceiling

--- format_sheets.py
+++ format_sheets.py
@@ -1,4 +1,9 @@
+import numpy as np
 import pandas as pd
+from functools import reduce
+import scipy.stats as ss
+from collections import defaultdict
+
 
 sites = ['rotogrinders', 'awesemo', 'sabersim']
 
@@ -44,20 +49,56 @@ def merge_projections_to_DK(site):
     new_sheet.drop(['Player', 'FPts'], axis=1, inplace=True)
     return new_sheet 
 
-def format_and_save(): # input site names separated by comma
-    for site in sites:
-        sheet = merge_projections_to_DK(site)
-        sheet.to_csv(f'{site}_projections.csv')
-        print(f"{site} projections saved.")
+def get_stdev():
+    df = pd.read_csv('sabersim_raw.csv').fillna(0)
+    df = df[['Name', 'dk_std']]
+    d = defaultdict(int)
+    for name, std in zip(df['Name'], df['dk_std']):
+        d[name] = round(std, 1)
+    return d
 
-# weights based on twitter popularity
-# rotogrinders 100k
-# labs 50k
-# awesemo 20k
-# fantasycruncher 15k
-# sabersim 10k
+def average_projections():
+    # average the projections from the sites and make ceilings column
+    # should probably pass site and go site by site
+    ssim = format_sabersim_projections()
+    rg = format_rotogrinders_projections()
+    aw = format_awesemo_projections()
+    std = get_stdev()
+    dfs = [ssim, rg, aw]
+    df = reduce(lambda left, right: pd.merge(left, right, on='Player'), dfs)
+    stds = []
+    means = []
+    ceils = []
+    for i,p in enumerate(df['Player']):
+        stds.append(std[p])
+        means.append(np.mean([df['FPts_x'][i], df['FPts_y'][i], df['FPts'][i]]))
+        ceils.append(ss.norm(means[i], std[p]).ppf(0.9))
+    df['means'] = means
+    df['std'] = stds 
+    df['ceil'] = ceils
+    return df[['Player', 'ceil']].dropna()
+
+def merge_ceilings_to_DK():
+    dk_sheet = pd.read_csv('dk_salaries.csv')
+    df = average_projections()
+    new_sheet = pd.merge(dk_sheet, df, left_on='Name', right_on='Player')
+    new_sheet['AvgPointsPerGame'] = new_sheet['ceil']
+    new_sheet.drop(['Player', 'ceil'], axis=1, inplace=True)
+    return new_sheet 
+
+def format_and_save(ceil=False): 
+    if ceil == True:
+        sheet = merge_ceilings_to_DK()
+        sheet.to_csv(f'ceilings.csv')
+        print(f"ceilings saved.")
+    else:
+        for site in sites:
+            sheet = merge_projections_to_DK(site)
+            sheet.to_csv(f'{site}_projections.csv')
+            print(f"{site} projections saved.")
+
 
 get_dk_salaries()
-format_and_save()
+format_and_save(ceil=True)
 
 
--- project_ownership.py
+++ project_ownership.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from pydfs_lineup_optimizer import get_optimizer, Site, Sport
 from collections import defaultdict
+import scipy.stats as ss
 
 sites = ['rotogrinders', 'awesemo', 'sabersim']
 weights = {'rotogrinders': 0.4, 'awesemo': 0.3, 'sabersim': 0.3}
@@ -17,6 +18,26 @@ def generate_lineups(site, n):
         lineup_list.append(lineup.salary_costs)
         lineups.append(lineup_list)
     return pd.DataFrame(lineups, columns=headers)
+
+def generate_ceilings_lineups(n):
+    optimizer = get_optimizer(Site.DRAFTKINGS, Sport.BASKETBALL)
+    optimizer.load_players_from_csv(f'ceilings.csv')
+    lineups = []
+    headers = ['PG', 'SG', 'SF', 'PF', 'C', 'G', 'F', 'UTIL', 'Pts', 'Salary']
+    for lineup in optimizer.optimize(n=n):
+        # create list of lineups with pts and salary tacked on
+        lineup_list = list(lineup.players)
+        lineup_list.append(lineup.fantasy_points_projection)
+        lineup_list.append(lineup.salary_costs)
+        lineups.append(lineup_list)
+    df = pd.DataFrame(lineups, columns=headers)
+    df.to_csv('ceilings_lineups.csv')
+    df = df[['PG', 'SG', 'SF', 'PF', 'C', 'G', 'F', 'UTIL']]
+    for col in df.columns:
+        df[col] = df[col].apply(lambda x: ' '.join(str(x).split()[:-2]))
+    df.to_csv('ceilings_upload.csv')
+    print(df)
+    return df
 
 def get_player_projections():
     df = pd.read_csv(f'{sites[0]}_projections.csv')
@@ -46,7 +67,7 @@ def get_stdev():
     df = df[['Name', 'dk_std']]
     d = defaultdict(int)
     for name, std in zip(df['Name'], df['dk_std']):
-        d[name] = int(std)
+        d[name] = round(std, 1)
     return d
 
 def calculate_exposure(): # input site names separated by comma, in order rotogrinders, fantasylabs, numberfire, sabersim
@@ -89,20 +110,25 @@ def calculate_exposure(): # input site names separated by comma, in order rotogr
     d3 = defaultdict(int)
     for name in d2.keys(): # strip names so they match projections
         stripped_name = ' '.join(name.split()[:-2])
-        d3[stripped_name] = [round(d2[name], 2), 
-                            round(projections[stripped_name] / (salaries[stripped_name]+0.1) * 1000, 2),
-                            round(projections[stripped_name]), salaries[stripped_name], 
-                            round(stdev[stripped_name] / (projections[stripped_name]+0.1) * 10, 2)]
+        proj = projections[stripped_name]
+        sal = salaries[stripped_name]
+        val = projections[stripped_name] / (salaries[stripped_name]+0.1) * 1000
+        std = stdev[stripped_name]
+        ceil = ss.norm(projections[stripped_name], stdev[stripped_name]).ppf(0.9)
+        d3[stripped_name] = [round(d2[name], 2), round(val, 2), round(proj, 2), sal, 
+                            round(std / (proj+0.1) * 10, 2), round(ceil, 2),
+                            round(ceil / sal * 1000, 2)]
     # convert back into DataFrame
     results = pd.DataFrame.from_dict(d3, orient='index', 
-              columns=['lineup_%', 'value', 'pts', 'salary', 'stdev/pts']).sort_values(by=['lineup_%'], 
+              columns=['lineup_%', 'value', 'pts', 'salary', 'stdev/pts', 'ceiling', 'ceiling value']).sort_values(by=['lineup_%'], 
               ascending=False)
     results['projected_ownership'] = round(-0.196651 + results['lineup_%']*0.160766 + results['value']*0.048588 + results['salary']*0.000013, 2)
     print(results)
     results.to_csv('ownership_projections.csv')
     print("Ownership projections saved.")
 
-calculate_exposure()
+#calculate_exposure()
+print(generate_ceilings_lineups(50))
 
 # add sabersim stdev column
 # test which weights work best
